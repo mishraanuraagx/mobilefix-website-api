a714a60fedb659b970492a1698c79451fe32a0ef
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-04-13 15:24:42 +0530

    initial Commit with only user class for now

---
+++ src/main/java/com/teamscreenbiz/core/DatabaseLoader.java
@@ -0,0 +1,41 @@
+package com.teamscreenbiz.core;
+
+
+import com.teamscreenbiz.user.User;
+import com.teamscreenbiz.user.UserRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DatabaseLoader implements ApplicationRunner{
+    private final UserRepository users;
+
+  public DatabaseLoader(UserRepository users) {
+    this.users = users;
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    List<User> students = Arrays.asList(
+//        String firstName, String lastName, String userName, String address,
+//        String password, String[] roles, boolean emailConfirmed, String email,
+//    boolean firstPhoneNumberConfirmed, Long firstPhoneNumber,
+//    boolean secondPhoneNumberConfirmed, Long secondPhoneNumber
+
+        new User("Anuraag", "Mishra",  "max", "death","#504 sec-38A Chandigarh", new String[] {"ROLE_USER","ROLE_ADMIN"},false,
+            "mishra.anuraag@yahoo.com",true,8146928380L,false,null)
+//        new User("Harsh", "Singh",  "hss", "eventful","#504 sec-38A Chandigarh", new String[] {"ROLE_USER"},false,
+//            "mishra.anuraag@yahoo.com",true,8146928380L,false,null),
+//        new User("Anuraag", "Mishra",  "max", "death","#504 sec-38A Chandigarh", new String[] {"ROLE_USER"},false,
+//            "mishra.anuraag@yahoo.com",true,8146928380L,false,null),
+//        new User("Anuraag", "Mishra",  "max", "death","#504 sec-38A Chandigarh", new String[] {"ROLE_USER"},false,
+//            "mishra.anuraag@yahoo.com",true,8146928380L,false,null)
+
+        );
+    users.save(students);
+  }
+}
---
+++ src/main/java/com/teamscreenbiz/user/User.java
@@ -0,0 +1,189 @@
+package com.teamscreenbiz.user;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.teamscreenbiz.core.BaseEntity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User extends BaseEntity{
+  public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+      Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+          Pattern.CASE_INSENSITIVE);
+
+  @NotNull
+  @Size(min = 5, max = 20)
+  private String firstName;
+
+
+  @Size(min = 5, max = 20)
+  private String lastName;
+
+  @NotNull
+  @Size(min = 3, max = 20)
+  private String userName;
+
+
+  private String address;
+
+  @NotNull
+//  @JsonIgnore
+//  @Size(min = 5, max = 16) /*Size property not working check*/
+  private String password;
+
+//  @JsonIgnore
+  private String[] roles;
+  boolean emailConfirmed;
+
+//TODO max: not working validation moved on to other tables for now
+  @Size(min = 3, max = 321)
+  private String email;
+
+  @JsonProperty(required = true)
+  boolean firstPhoneNumberConfirmed;
+  private Long firstPhoneNumber;
+
+
+
+  boolean secondPhoneNumberConfirmed;
+  //TODO max: check for hypenated mobile number
+  private Long secondPhoneNumber;
+
+
+
+  protected User() {super();}
+
+  public User(String firstName, String lastName, String userName, String address,
+              String password, String[] roles, boolean emailConfirmed, String email,
+              boolean firstPhoneNumberConfirmed, Long firstPhoneNumber,
+              boolean secondPhoneNumberConfirmed, Long secondPhoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.address = address;
+    this.password = password;
+    this.roles = roles;
+    this.emailConfirmed = emailConfirmed;
+    this.email = email;
+    this.firstPhoneNumberConfirmed = firstPhoneNumberConfirmed;
+    this.firstPhoneNumber = firstPhoneNumber;
+    this.secondPhoneNumberConfirmed = secondPhoneNumberConfirmed;
+    this.secondPhoneNumber = secondPhoneNumber;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String[] getRoles() {
+    return roles;
+  }
+
+  public void setRoles(String[] roles) {
+    this.roles = roles;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public boolean isEmailConfirmed() {
+    return emailConfirmed;
+  }
+
+  public void setEmailConfirmed(boolean emailConfirmed) {
+    this.emailConfirmed = emailConfirmed;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    if(validate(email)){
+      this.email = email;
+      //TODO max: set message for successfull email update
+    }
+
+
+  }
+
+
+  public static boolean validate(String emailStr) {
+    Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+    return matcher.find();
+  }
+
+  public Long getFirstPhoneNumber() {
+    return firstPhoneNumber;
+  }
+
+  public void setFirstPhoneNumber(Long firstPhoneNumber) {
+    this.firstPhoneNumber = firstPhoneNumber;
+  }
+
+  public boolean isFirstPhoneNumberConfirmed() {
+    return firstPhoneNumberConfirmed;
+  }
+
+  public void setFirstPhoneNumberConfirmed(boolean firstPhoneNumberConfirmed) {
+    this.firstPhoneNumberConfirmed = firstPhoneNumberConfirmed;
+  }
+
+  public boolean isSecondPhoneNumberConfirmed() {
+    return secondPhoneNumberConfirmed;
+  }
+
+  public void setSecondPhoneNumberConfirmed(boolean secondPhoneNumberConfirmed) {
+    this.secondPhoneNumberConfirmed = secondPhoneNumberConfirmed;
+  }
+
+  public Long getSecondPhoneNumber() {
+    return secondPhoneNumber;
+  }
+
+  public void setSecondPhoneNumber(Long secondPhoneNumber) {
+    this.secondPhoneNumber = secondPhoneNumber;
+  }
+
+
+
+}
