21ec0a5a4069d640e674c2d588e7c5bdb66c2683
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-05-06 20:51:38 +0530

    added more TODO, need to remove TODOs that i've already fixed :D

--- src/main/java/com/teamscreenbiz/Application.java
+++ src/main/java/com/teamscreenbiz/Application.java
@@ -13,5 +13,7 @@ public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+// TODO max: control the + (add) power on each details.html
+// TODO max: fix the no. of TODOs that u already fixed but r still visible xD :D
 
 }
